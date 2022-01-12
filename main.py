e063b8e7abea8bab663a9820da9790ede86adf99
Ksenia <kitten1508@mail.ru>
2022-01-12 10:59:47 +0300

    T ready

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def a(buf):
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+    
+def T(buf):
+    buf[0] = buf[0]+" ___    "
+    buf[1] = buf[1]+"  |     "
+    buf[2] = buf[2]+"  |     "
+    
 s = ["","","","","","","",""]
-a(s)
-a(s)
+T(s)
+T(s)
 print(*s, sep="\n")
