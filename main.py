600f66a4bca1cd2d10ef3cad41b53103f0687483
Igosheva Daria <darya.igosheva@gmail.com>
2022-01-12 11:23:43 +0300

    Q ready

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def a(buf):
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+    
+def Q(buf):
+    buf[0]=buf[0]+" __      "
+    buf[1]=buf[1]+"|  |     "
+    buf[2]=buf[2]+" --\     "  
 s = ["","","","","","","",""]
-a(s)
-a(s)
+Q(s)
+Q(s)
 print(*s, sep="\n")
