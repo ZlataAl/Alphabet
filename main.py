88e88e77c4bb6a85f09e80f7967a065024fe17d7
Ksenia <kitten1508@mail.ru>
2022-01-12 11:26:31 +0300

    E ready

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def a(buf):
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+def E(buf):
+    buf[0] = buf[0]+"  _     "
+    buf[1] = buf[1]+" |_     "
+    buf[2] = buf[2]+" |_     "
+
 s = ["","","","","","","",""]
-a(s)
-a(s)
+E(s)
+E(s)
 print(*s, sep="\n")
