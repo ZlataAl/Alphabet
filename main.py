b948ffcffe94865b62c8e37d84fe0e8bcb23bb2c
Igosheva Daria <darya.igosheva@gmail.com>
2022-01-12 11:42:31 +0300

    Y ready

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def a(buf):
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+    
+def Y(buf):
+    buf[0] = buf[0]+"\ /     "
+    buf[1] = buf[1]+" |      "
+    buf[2] = buf[2]+" |      "   
 s = ["","","","","","","",""]
-a(s)
-a(s)
+Y(s)
+Y(s)
 print(*s, sep="\n")
