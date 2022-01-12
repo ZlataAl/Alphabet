437b5ed28df8d64f16125256dcc2dd6087a03f1d
Igosheva Daria <darya.igosheva@gmail.com>
2022-01-12 10:54:32 +0300

    C ready

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def a(buf):
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+def C(buf):
+    buf[0]=buf[0]+" _      "
+    buf[1]=buf[1]+"|       "
+    buf[2]=buf[2]+"|_      "
 s = ["","","","","","","",""]
-a(s)
-a(s)
+C(s)
+C(s)
 print(*s, sep="\n")
