16d51444f8df1f2771ff854facffe02f97bd713a
Zlata <Zlataalekseeva17@gmail.com>
2022-01-12 10:54:46 +0300

    R ready

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+def R(buf):
+    buf[0] = buf[0]+"|)      "
+    buf[1] = buf[1]+"|\      "
+    buf[2] = buf[2]+"        "
+s = ["","","","","","","",""]
+R(s)
+R(s)
+print(*s, sep="\n")
