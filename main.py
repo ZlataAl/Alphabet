d3ab665daa8f10be39ecc3843c8e14f215be1123
Zlata <Zlataalekseeva17@gmail.com>
2022-01-12 10:11:48 +0300

    p ready

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+def p(buf):
+    buf[0] = buf[0]+"|)      "
+    buf[1] = buf[1]+"|       "
+    buf[2] = buf[2]+"        "
+s = ["","","","","","","",""]
+p(s)
+p(s)
+print(*s, sep="\n")
