09357c3ba6b29c34b5c17791a8a11655bda722d1
Ksenia <kitten1508@mail.ru>
2022-01-12 10:54:09 +0300

    M ready

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def a(buf):
+def a(buf):            
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+    
+def M(buf):
+    buf[0] = buf[0]+" |\/|   "
+    buf[1] = buf[1]+" |  |   "
+    buf[2] = buf[2]+"        "
+    
 s = ["","","","","","","",""]
-a(s)
-a(s)
+M(s)
+M(s)
 print(*s, sep="\n")
