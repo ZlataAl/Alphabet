1f5c710d2421f50855ac6ee63bd536f80430832a
Ksenia <kitten1508@mail.ru>
2022-01-12 11:21:28 +0300

    J ready

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ def a(buf):
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+    
+def J(buf):
+    buf[0] = buf[0]+" __     "
+    buf[1] = buf[1]+"  |     "
+    buf[2] = buf[2]+" _/     "
+
+    
 s = ["","","","","","","",""]
-a(s)
-a(s)
+J(s)
+J(s)
 print(*s, sep="\n")
