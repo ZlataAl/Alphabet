c94fc2d4f1431b4bb78ce6aab9ed781b91ef8d59
Igosheva Daria <darya.igosheva@gmail.com>
2022-01-12 11:18:29 +0300

    S ready

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def a(buf):
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+    
+def S(buf):
+    buf[0] = buf[0]+" _      "
+    buf[1] = buf[1]+"(_      "
+    buf[2] = buf[2]+"__)     " 
 s = ["","","","","","","",""]
-a(s)
-a(s)
+S(s)
+S(s)
 print(*s, sep="\n")
