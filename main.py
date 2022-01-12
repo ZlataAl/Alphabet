a3b9a79f447029a9c5c26d534e7abd42a80c3f7a
Igosheva Daria <darya.igosheva@gmail.com>
2022-01-12 10:35:11 +0300

    D ready

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def a(buf):
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+    
+def D(buf):
+    buf[0] = buf[0]+"|\      "
+    buf[1] = buf[1]+"| |     "
+    buf[2] = buf[2]+"|/      "
+
 s = ["","","","","","","",""]
-a(s)
-a(s)
+D(s)
+D(s)
 print(*s, sep="\n")
