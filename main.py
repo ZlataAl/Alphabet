12228a2814926b40545f9d195a58ab116f3077c0
Ksenia <kitten1508@mail.ru>
2022-01-12 11:02:45 +0300

    N ready

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def a(buf):
     buf[0] = buf[0]+" /\     "
     buf[1] = buf[1]+"/--\    "
     buf[2] = buf[2]+"        "
+    
+def N(buf):
+    buf[0] = buf[0]+" |\ |   "
+    buf[1] = buf[1]+" | \|   "
+    buf[2] = buf[2]+"        "
+    
 s = ["","","","","","","",""]
-a(s)
-a(s)
+N(s)
+N(s)
 print(*s, sep="\n")
