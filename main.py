823cc17d324dff1f785972434626315b28d74489
Zlata <Zlataalekseeva17@gmail.com>
2022-01-12 10:03:07 +0300

    a ready

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+def a(buf):
+    buf[0] = buf[0]+" /\     "
+    buf[1] = buf[1]+"/--\    "
+    buf[2] = buf[2]+"        "
+s = ["","","","","","","",""]
+a(s)
+a(s)
+print(*s, sep="\n")
